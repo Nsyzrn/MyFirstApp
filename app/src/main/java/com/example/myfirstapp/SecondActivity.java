75cc32ff2d090af65ff3f0702ee0808aaf971eb2
Nsyzrn <nr.syazrina88@gmail.com>
2022-10-12 14:55:26 +0800

    MyFirstApps

--- app/src/main/java/com/example/myfirstapp/SecondActivity.java
+++ app/src/main/java/com/example/myfirstapp/SecondActivity.java
@@ -9,16 +9,27 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     public final static String MESSAGE_KEY = "com.example.MyFirstApp.message_key";
+    public final static String MESSAGE_KEY_Address = "com.example.MyFirstApp.Address";
+    public final static String MESSAGE_KEY_Phone = "com.example.MyFirstApp.Phone";
+    public final static String MESSAGE_KEY_Date= "com.example.MyFirstApp.Date";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
+
         String message = intent.getStringExtra(MESSAGE_KEY);
+        String message1 = intent.getStringExtra(MESSAGE_KEY_Date);
+        String message2 = intent.getStringExtra(MESSAGE_KEY_Address);
+        String message3 = intent.getStringExtra(MESSAGE_KEY_Phone);
+
         TextView textView = new TextView(this);
-        textView.setTextSize(45);
-        textView.setText(message);
+
+        textView.setTextSize(20);
+
+        textView.setText("Name: "+ message +"\nBirth Date: "+ message1 + "\nAddress: "+ message2 +"\nPhone: "+ message3);
+
         setContentView(textView);
     }
 }
